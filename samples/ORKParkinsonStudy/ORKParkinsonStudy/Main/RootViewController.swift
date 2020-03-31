9a357ddc5e88743df35c6c3d4399a4a3efebe82d
John R. Howell <john_howell@byu.edu>
2020-03-31 14:26:51 -0600

    Updated to iOS 13.4 and XCode 11.4

--- samples/ORKParkinsonStudy/ORKParkinsonStudy/Main/RootViewController.swift
+++ samples/ORKParkinsonStudy/ORKParkinsonStudy/Main/RootViewController.swift
@@ -84,7 +84,7 @@ class RootViewController: UIViewController, OnboardingManagerDelegate {
 }
 
 public extension UIImage {
-    public convenience init?(color: UIColor) {
+    convenience init?(color: UIColor) {
         let rect = CGRect(origin: .zero, size: CGSize(width: 1, height: 1))
         UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
         color.setFill()
